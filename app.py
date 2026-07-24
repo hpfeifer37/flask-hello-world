2dd2cdc54cd07238ddcf6f1f910cb5dabdaca23f
Hannah Pfeifer <hannahlpfeifer@gmail.com>
2026-07-24 19:44:10 +0000

    Added /db_test route

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 @app.route('/')
 def hello_world():
     return 'Hello World from Hannah in 3308!'
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Database connection successful"
+    except Exception as e:
+        return f"Database connection failed: {e}"
+    finally:
+        if conn is not None:
+            conn.close()
