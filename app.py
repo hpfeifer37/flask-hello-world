1f75ef15830bdd185079b5f7a40427f9ec9a00f8
Hannah Pfeifer <hannahlpfeifer@gmail.com>
2026-07-24 20:04:10 +0000

    Added /db_insert route

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def db_test():
         if conn is not None:
             conn.close()
 
+
 @app.route("/db_create")
 def db_create():
     conn = None
@@ -38,12 +39,44 @@ def db_create():
                 Last varchar(255),
                 City varchar(255),
                 Name varchar(255),
-                Number int
-            );
+                Number int);
         """)
         conn.commit()
         
         return "Basketball Table Created"
+        
+    except Exception as e:
+        if conn is not None:
+            conn.rollback()
+        return f"Database error: {e}"
+        
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+
+@app.route("/db_insert")
+def db_insert():
+    conn = None
+    cur = None
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        cur.execute("""
+            INSERT INTO Basketball (First, Last, City, Name, Number)
+            VALUES
+                ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+                ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+                ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+                ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2),
+                ('Hannah', 'Pfeifer', 'CU Boulder', 'Team Infinity', 3308);
+        """)
+        conn.commit()
+        
+        return "Basketball Table Populated"
         
     except Exception as e:
         if conn is not None:
